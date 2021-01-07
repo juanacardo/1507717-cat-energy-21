c41c564d5a0a647f6ad5339ce066288febc55e26
juanacardo <juanacardo@gmail.com>
2021-01-07 17:44:57 +0300

    Завершаем верстку главной страницы

---
+++ source/js/script.js
@@ -0,0 +1,14 @@
+const navMain = document.querySelector('.main-navigation');
+const navToggle = document.querySelector('.main-navigation__toggle');
+
+navMain.classList.remove('main-navigation--no-js');
+
+navToggle.addEventListener('click', function() {
+  if (navMain.classList.contains('main-navigation--closed')) {
+    navMain.classList.remove('main-navigation--closed');
+    navMain.classList.add('main-navigation--opened');
+  } else {
+    navMain.classList.add('main-navigation--closed');
+    navMain.classList.remove('main-navigation--opened');
+  }
+});
